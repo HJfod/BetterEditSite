b86c6bc25529a3a16a10f6fd0aceaf23d096b4e2
HJfod <60038575+HJfod@users.noreply.github.com>
2024-08-10 23:18:44 +0300

    update bakery

--- src/routes/bakery/+page.svelte
+++ src/routes/bakery/+page.svelte
@@ -1,9 +1,42 @@
 <script lang="ts">
     import Button from "$lib/components/Button.svelte";
+    import LoadingCircle from "$lib/components/LoadingCircle.svelte";
 
     let eggCount: string;
     let cakeName: string;
     let resultP: HTMLParagraphElement;
+    let loading = false;
+
+    async function bake() {
+        try {
+            loading = true;
+            const result = await fetch(
+                "https://api.betteredit.pro/new-license",
+                {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        supported_amount: parseInt(eggCount),
+                        token: cakeName,
+                    }),
+                }
+            );
+            if (!result.ok) {
+                throw result.status;
+            }
+            const json = await result.json() as {
+                created_license_key: string,
+            };
+            loading = false;
+            resultP.innerHTML = json.created_license_key;
+        }
+        catch(err: any) {
+            loading = false;
+            resultP.innerHTML = err === 400 ? 'bad cake' : err.toString();
+        }
+    }
 </script>
 
 <h1>tasty cake bakery mmm</h1>
@@ -14,30 +47,9 @@
 <p>whats the name of the cake</p>
 <input bind:value={cakeName}>
 
-<p bind:this={resultP}></p>
+{#if loading}
+    <LoadingCircle/>
+{/if}
+<p bind:this={resultP} style={loading ? "display: none;" : ""}></p>
 
-<Button on:click={async () => {
-    try {
-        const result = await fetch(
-            "https://api.betteredit.pro/new-license",
-            {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    supported_amount: parseInt(eggCount),
-                    token: cakeName,
-                }),
-            }
-        );
-        const json = await result.json();
-        if (json.created_license_key) {
-            resultP.innerHTML = json.created_license_key;
-        }
-        else throw json.toString();
-    }
-    catch(err) {
-        resultP.innerHTML = `bad cake: ${err}`;
-    }
-}}>bake the cake</Button>
+<Button on:click={bake}>bake the cake</Button>
